711e5e4c4b802a29cbc5d99581b5e0351111e1a6
hgrafa <hugorafell@gmail.com>
2022-08-13 14:19:30 -0300

    update aula 13/08/22

---
+++ C samples/hello.c
@@ -0,0 +1,18 @@
+// incluindo 
+// std = standard/padrão + 
+// i = input/entrada
+// o = output/saída
+// .h = header/biblioteca
+#include <stdio.h>
+
+int main() { // main = principal
+
+  int a = 2;
+  int b = 3;
+  printf("ola\n");
+  printf("primeiro valor digitado = %d\n", a);
+  printf("segundo valor digitado = %d\n", b);
+  printf("soma = %d\n", a+b);
+
+  return 0;
+}
