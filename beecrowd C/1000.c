2b8965b065aca1c8cc135a971d3cc0a9b761d46e
hgrafa <hugorafell@gmail.com>
2022-08-27 13:06:51 -0300

    adding notes aula 27/08/22

---
+++ beecrowd C/1000.c
@@ -0,0 +1,13 @@
+
+// incluindo bibliotecas
+#include <stdio.h>
+
+// funcao principal
+int main() {
+
+  //  imprimindo o conteudo
+  printf("Hello World!\n");
+
+  //  fim do programa
+  return 0;
+}
---
+++ beecrowd C/1001.c
@@ -0,0 +1,19 @@
+
+// incluindo biliotecas
+#include <stdio.h>
+
+// funcao principal
+int main() {
+
+  // criando dois inteiros
+  int a, b; 
+
+  // lendo dois numeros inteiros
+  scanf("%d %d", &a, &b);
+
+  // imprindo o resultado
+  printf("X = %d\n", a+b);
+
+  // fim do programa
+  return 0;
+}
