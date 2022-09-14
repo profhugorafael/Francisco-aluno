760a82eca5fa29e10bb1b2b7bc327b64af842981
hgrafa <hugorafell@gmail.com>
2022-09-14 09:56:14 -0300

    Resolvendo problema 1020

---
+++ beecrowd C/1020.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+  int dias;
+
+  scanf("%d", &dias);
+
+  int anos = (dias/365);
+  dias = dias - (anos*365);
+
+  int meses = (dias/30);
+  dias = dias - (meses*30);
+
+  printf("%d ano(s)\n", anos);
+  printf("%d mes(es)\n", meses);
+  printf("%d dia(s)\n", dias);
+
+  return 0;
+}
